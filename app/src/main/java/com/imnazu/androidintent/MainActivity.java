70f61d37ddeca174a77fd3ed9cae685b415023f8
naimazulfa <naima.zulfa@students.amikom.ac.id>
2021-10-22 15:28:54 +0700

    Initial commit

---
+++ app/src/main/java/com/imnazu/androidintent/MainActivity.java
@@ -0,0 +1,41 @@
+package com.imnazu.androidintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    final public static String EXTRA_NAME = "extra_name";
+    final public static String EXTRA_EMAIL = "extra_email";
+    final public static String EXTRA_PHONE = "extra_phone";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText inputName = (EditText) findViewById(R.id.idInputName);
+        EditText inputEmail = (EditText) findViewById(R.id.idInputEmail);
+        EditText inputPhone = (EditText) findViewById(R.id.idInputPhone);
+        EditText inputPassword = (EditText) findViewById(R.id.idInputPassword);
+
+        Button btnSave = (Button) findViewById(R.id.inButtonSave);
+        btnSave.setOnClickListener(v ->{
+            Intent loginIntent = new Intent(MainActivity.this, DashboardActivity.class);
+            loginIntent.putExtra(EXTRA_NAME, inputName.getText().toString());
+            loginIntent.putExtra(EXTRA_EMAIL, inputEmail.getText().toString());
+            loginIntent.putExtra(EXTRA_PHONE, inputPhone.getText().toString());
+            startActivity(loginIntent);
+
+            //Log.d("--Nama--", inputName.getText().toString());
+            //Log.d("--Email--", inputEmail.getText().toString());
+            //Log.d("--Phone--", inputPhone.getText().toString());
+            //Log.d("--Password--", inputPassword.getText().toString());
+        });
+    }
+}
